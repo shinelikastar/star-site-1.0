911f7277938948e434284c5ebe3fa05c4e814b1d
star <star_su@brown.edu>
2020-07-12 11:36:41 -0400

    WIP hooking up config to navbar rendering

---
+++ README.md
@@ -0,0 +1,5 @@
+### TODO
+
+- [ ] Render circular icon for navbar link
+- [ ] Map config to navbar rendering
+- [ ] Setup `onClick` and `onHover` handlers, hook up to `active` prop
--- star-site/src/App.js
+++ star-site/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import logo from "./logo.svg";
-import NavbarLink from "../src/NavbarLink";
+import NavbarLink from "./Navbar/NavbarLink";
 import "./App.css";
 
 function App() {
