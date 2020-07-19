90bfc2a264f9acc19b6b33c58d7df48736bc3401
star <star_su@brown.edu>
2020-07-18 23:11:36 -0400

    landing page done

--- star-site/src/App.js
+++ star-site/src/App.js
@@ -4,12 +4,20 @@ import { createBrowserHistory } from "history";
 
 import Navbar from "./Navbar/Navbar";
 import config from "./Navbar/navbar_config";
+import girl from "./assets/diva.png";
 import "./App.css";
 
 function App() {
   const innerApp = (
     <div className="App">
       <header className="App-header">
+        <div className="Girl-container">
+          <img src={girl} className="Girl-diva" />
+          <div className="Welcome-container">
+            <p className="DisplayText">hi, it's star!</p>
+            <p>welcome 2 an online representation of me</p>
+          </div>
+        </div>
         <Navbar config={config}></Navbar>
       </header>
     </div>
@@ -20,8 +28,12 @@ function App() {
   return (
     <Router history={history}>
       <Switch>
-        <Route path="/">{innerApp}</Route>
-        <Route path="/about">{innerApp}</Route>
+        <Route path="/" component={App}>
+          {innerApp}
+        </Route>
+        <Route path="/about" component={App}>
+          {innerApp}
+        </Route>
         <Route path="/work">{innerApp}</Route>
         <Route path="/writing">{innerApp}</Route>
       </Switch>
