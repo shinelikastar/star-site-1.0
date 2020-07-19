333037b1ab7f644b98642f22005f7a044b053711
star <star_su@brown.edu>
2020-07-19 17:22:52 -0400

    first project display done, before refactor into sleek components

--- star-site/src/PageComponents/WorkPage/Project.js
+++ star-site/src/PageComponents/WorkPage/Project.js
@@ -1,10 +1,13 @@
 import React from "react";
 import classNames from "classnames";
 import { motion } from "framer-motion";
+import { GoOctoface } from "react-icons/go";
 
 import weenixStatic from "./images/weenix/weenix_static.png";
 
 import "./Project.css";
+
+const githubBlue = "rgb(16, 162, 245)";
 
 class Project extends React.Component {
   renderImage = () => {
@@ -18,10 +21,14 @@ class Project extends React.Component {
   renderBoard = () => {
     return (
       <div className="Project-board">
-        <p>
+        <p className="Project-header">Weenix</p>
+        <p className="Project-text">
           Unix-like kernel built as a semester long project for Operating
-          Systesm (CS167/690)
+          Systems (CS167/690)
         </p>
+        <a href="https://github.com/brown-cs1690/weenix-2020-shinelikastar">
+          <GoOctoface size={30} fill={githubBlue} className="Project-icon" />
+        </a>
       </div>
     );
   };
---
+++ star-site/src/PageComponents/WorkPage/project_config.js
@@ -0,0 +1,13 @@
+export default [
+  {
+    title: "weenix",
+    description:
+      "Unix-like kernel built as a semester long project for Operating Systems (CS167/690)",
+    links: {
+      github: "https://github.com/brown-cs1690/weenix-2020-shinelikastar",
+    },
+    imgs: {
+      static: "./images/weenix/weenix_static.png",
+    },
+  },
+];
