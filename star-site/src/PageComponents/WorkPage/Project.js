02cf11bd7598adb3cf20f7313797657bbc29735f
star <star_su@brown.edu>
2020-07-19 14:30:38 -0400

    infra for projects page

---
+++ star-site/src/PageComponents/WorkPage/Project.js
@@ -0,0 +1,17 @@
+import React from "react";
+import classNames from "classnames";
+import { motion } from "framer-motion";
+
+import "./Project.css";
+
+class Project extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <p>incoming projects!</p>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Project;
--- star-site/src/PageComponents/WorkPage/WorkPage.js
+++ star-site/src/PageComponents/WorkPage/WorkPage.js
@@ -4,14 +4,31 @@ import { motion } from "framer-motion";
 
 import Navbar from "../../Navbar/Navbar";
 import config from "../../Navbar/navbar_config";
+import Project from "./Project";
 
 import "./WorkPage.css";
 
 class WorkPage extends React.Component {
+  renderHeader = () => {
+    const headerClassText = classNames("Header", "Header-text");
+    const headerClass = classNames("Header", "Header-container");
+    const headerText = "some things i've built...";
+    return (
+      <div className={headerClass}>
+        <p className={headerClassText}>{headerText}</p>
+      </div>
+    );
+  };
+
+  renderProjects = () => {
+    return <Project></Project>;
+  };
+
   render() {
     return (
       <React.Fragment>
-        <div>Hello!</div>
+        {this.renderHeader()}
+        {this.renderProjects()}
         <Navbar config={config}></Navbar>
       </React.Fragment>
     );
