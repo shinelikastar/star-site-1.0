72b92f36cffb093d9752568e68a0f99bd38ccf86
star <star_su@brown.edu>
2020-07-26 22:00:28 -0400

    header for WritingPage done

--- star-site/src/PageComponents/WritingPage/index.js
+++ star-site/src/PageComponents/WritingPage/index.js
@@ -1,6 +1,19 @@
 import React from "react";
 import classNames from "classnames";
 
+import Header from "../Header";
 import "./WritingPage.css";
 
-class WritingPage extends React.Component {}
+class WritingPage extends React.Component {
+  render() {
+    const headerText = "some things i've written";
+    return (
+      <React.Fragment>
+        {" "}
+        <Header text={headerText} />
+      </React.Fragment>
+    );
+  }
+}
+
+export default WritingPage;
