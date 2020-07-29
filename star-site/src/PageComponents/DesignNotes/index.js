7489efb19b003af86a57d11ab2120a21440e381c
star <star_su@brown.edu>
2020-07-29 00:22:47 -0400

    adding design thoughts

--- star-site/src/PageComponents/DesignNotes/index.js
+++ star-site/src/PageComponents/DesignNotes/index.js
@@ -4,9 +4,18 @@ import "./DesignNotes.css";
 import Header from "../../shared-components/Header";
 
 function DesignNotes() {
+  function renderNotes() {
+    return (
+      <p>
+        Space to give a shoutout to all the wonderful things that have
+        intersected in the creation of this website.
+      </p>
+    );
+  }
+
   return (
     <div className="DesignNotes-container">
-      <Header text="some thoughts on design 🌿"></Header>
+      <Header text="some thoughts on design 🌿">{renderNotes()}</Header>
     </div>
   );
 }
